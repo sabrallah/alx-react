ff79bd61d79851a15207822990c04d59d5687b34
sabrallah youssef <58825588+sabrallah@users.noreply.github.com>
2024-02-13 04:45:07 +0000

    update

--- 0x04-React_inline_styling/task_1/dashboard/src/App/App.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/App/App.js
@@ -32,7 +32,7 @@ class App extends React.Component {
   handleKeyPress (e) {
     if (e.ctrlKey && e.key === 'h') {
       e.preventDefault()
-      alert('Logging you out')
+      window.alert('Logging you out')
       this.props.logOut()
     }
   }
