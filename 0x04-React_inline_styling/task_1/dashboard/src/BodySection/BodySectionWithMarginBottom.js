6257797d0f844419d9e61436798c2649a3cc0d3e
sabrallah youssef <58825588+sabrallah@users.noreply.github.com>
2024-02-13 04:27:53 +0000

    upda

--- 0x04-React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,27 +1,27 @@
-import PropTypes from "prop-types";
-import React, { Component } from "react";
-import BodySection from "./BodySection";
-import { StyleSheet, css } from "aphrodite";
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import BodySection from './BodySection'
+import { StyleSheet, css } from 'aphrodite'
 
 class BodySectionWithMarginBottom extends Component {
-  render() {
+  render () {
     return (
       <div className={css(styles.bodySectionWithMargin)}>
         <BodySection {...this.props} />
       </div>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   bodySectionWithMargin: {
-    marginBottom: "40",
-  },
-});
+    marginBottom: '40'
+  }
+})
 
 BodySectionWithMarginBottom.propTypes = {
   title: PropTypes.string.isRequired,
-  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
-};
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element])
+}
 
-export default BodySectionWithMarginBottom;
+export default BodySectionWithMarginBottom
