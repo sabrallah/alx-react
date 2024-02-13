348a792d4b66e266f35b62692a92eb31be585451
sabrallah youssef <58825588+sabrallah@users.noreply.github.com>
2024-02-13 04:32:27 +0000

    update

--- 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -1,31 +1,31 @@
-import React from "react";
-import logo from "../assets/holberton-logo.jpg";
-import { StyleSheet, css } from "aphrodite";
+import React from 'react'
+import logo from '../assets/holberton-logo.jpg'
+import { StyleSheet, css } from 'aphrodite'
 
-function Header() {
+function Header () {
   return (
     <>
-      <div className={css(styles["App-header"])}>
-        <img src={logo} className={css(styles.img)} alt="logo" />
+      <div className={css(styles['App-header'])}>
+        <img src={logo} className={css(styles.img)} alt='logo' />
         <h1>School dashboard</h1>
       </div>
     </>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  "App-header": {
-    fontSize: "1.4rem",
-    color: "#e0354b",
-    display: "flex",
-    alignItems: "center",
-    borderBottom: "3px solid #e0354b",
+  'App-header': {
+    fontSize: '1.4rem',
+    color: '#e0354b',
+    display: 'flex',
+    alignItems: 'center',
+    borderBottom: '3px solid #e0354b'
   },
 
   img: {
-    width: "200px",
-    height: "200px",
-  },
-});
+    width: '200px',
+    height: '200px'
+  }
+})
 
-export default Header;
+export default Header
