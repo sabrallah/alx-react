ef40ea8d47574d773daabc5b416f8f3229fe26cf
sabrallah youssef <58825588+sabrallah@users.noreply.github.com>
2024-02-15 15:57:33 +0000

    update

--- 0x05-react_state/task_0/dashboard/src/BodySection/BodySection.test.js
+++ 0x05-react_state/task_0/dashboard/src/BodySection/BodySection.test.js
@@ -1,1 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
 import BodySection from './BodySection';
+
+describe('<BodySection />', () => {
+    it('renders without crashing', () => {
+        shallow(<BodySection />);
+    });
+});
