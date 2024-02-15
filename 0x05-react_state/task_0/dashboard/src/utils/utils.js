157fa7f86004995589e98411cda24708b856dfd4
sabrallah youssef <58825588+sabrallah@users.noreply.github.com>
2024-02-15 21:03:55 +0000

    update

--- 0x05-react_state/task_0/dashboard/src/utils/utils.js
+++
@@ -1,9 +0,0 @@
-export function getFullYear() {
-  return new Date().getFullYear();
-}
-
-export function getFooterCopy(isIndex) {
-  return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
-}
-
-export const getLatestNotification = () => '<strong>Urgent requirement</strong> - complete by EOD';
