f7bfc4e67976569d469bc3274d847511e6239533
sabrallah youssef <58825588+sabrallah@users.noreply.github.com>
2024-02-16 06:51:46 +0000

    update

--- 0x05-react_state/task_0/dashboard/src/BodySection/BodySection.test.js
+++ 0x05-react_state/task_0/dashboard/src/BodySection/BodySection.test.js
@@ -1,1 +1,24 @@
-BodySection.js 
+import React from 'react';
+import { shallow } from 'enzyme';
+import BodySection from './BodySection';
+import { StyleSheetTestUtils } from 'aphrodite';
+
+StyleSheetTestUtils.suppressStyleInjection();
+
+
+describe('<BodySection />', () => {
+    it('should render correctly the children', () => {
+        const wrapper = shallow(
+            <BodySection title="test title">
+                <p>test children node</p>
+            </BodySection>
+        );
+
+        expect(wrapper.find('h2')).toHaveLength(1);
+        expect(wrapper.find('h2').text()).toContain('test title');
+
+        expect(wrapper.find('p')).toHaveLength(1);
+        expect(wrapper.find('p').text()).toContain('test children node');
+
+    });
+});
