a738847ab8d8bb31fdad2b9d8153c1eac2345a4b
sabrallah91 <sabrallah1991@gmail.com>
2024-02-21 16:51:04 +0100

    Update 7-equality.js

--- 0x06-React_Immutable/7-equality.js
+++ 0x06-React_Immutable/7-equality.js
@@ -1,5 +1,12 @@
 import { is } from 'immutable';
 
+/**
+ * Function to check if two maps are equal.
+ *
+ * @param {Map} map1 - The first map to compare
+ * @param {Map} map2 - The second map to compare
+ * @return {boolean} Whether the two maps are equal
+ */
 export default function areMapsEqual(map1, map2) {
   return is(map1, map2);
 }
