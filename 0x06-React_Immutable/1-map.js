c554293f2c877dff564b26e43fcb3631664e5516
sabrallah91 <sabrallah1991@gmail.com>
2024-02-21 16:12:40 +0100

    Update 1-map.js

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,5 +1,9 @@
-import { Map } from 'immutable';
-
+/**
+ * Converts a regular object to an Immutable Map.
+ * 
+ * @param {Object} object - The object to be converted to an Immutable Map.
+ * @returns {Map} - An Immutable Map representation of the input object.
+ */
 const getImmutableObject = (object) => Map(object);
 
 export default getImmutableObject;
