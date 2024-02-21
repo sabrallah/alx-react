18a2849ead03a6df663b693dbc1ee322130fc7b2
sabrallah91 <sabrallahyoussef@gmail.com>
2024-02-21 15:37:00 +0100

    Update 1-map.js

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,12 +1,5 @@
 import { fromJS } from 'immutable';
 
-/**
- * Accesses an immutable object using the provided array of keys.
- *
- * @param {Object} object - The immutable object to access
- * @param {Array} array - The array of keys to access the object
- * @return {*} The value located at the specified keys in the object, or undefined if not found
- */
 export default function accessImmutableObject(object, array) {
   const mappedObj = fromJS(object);
 
