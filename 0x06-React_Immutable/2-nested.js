1568acbf8ff11f650a8ee8d687e6de8805139858
sabrallah91 <sabrallah1991@gmail.com>
2024-02-21 16:29:37 +0100

    Update 2-nested.js

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,7 +1,14 @@
 import { fromJS } from 'immutable';
 
+/**
+ * Accesses nested properties in an Immutable object
+ * @param {Object} object - The Immutable object to access
+ * @param {Array} array - An array representing the path to the nested property
+ * @returns The value of the nested property or undefined if not found
+ */
 export default function accessImmutableObject(object, array) {
   const mappedObj = fromJS(object);
 
   return mappedObj.getIn(array, undefined);
 }
+
