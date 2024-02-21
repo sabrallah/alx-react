0a36868fba9767924d2c9576ccc8b8cf7f02e699
sabrallah91 <sabrallahyoussef@gmail.com>
2024-02-21 15:43:15 +0100

    Update 1-map.js

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,7 +1,7 @@
-import { fromJS } from 'immutable';
+import { Map } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-  const mappedObj = fromJS(object);
+  const mappedObj = Map(object);
 
   return mappedObj.getIn(array, undefined);
 }
