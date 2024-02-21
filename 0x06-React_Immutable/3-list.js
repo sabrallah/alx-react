8a5dd52eaeca4a59850c9df8a0005c5288dcdbc5
sabrallah91 <sabrallah1991@gmail.com>
2024-02-21 16:34:32 +0100

    Update 3-list.js

--- 0x06-React_Immutable/3-list.js
+++ 0x06-React_Immutable/3-list.js
@@ -1,8 +1,21 @@
 import { List } from 'immutable';
 
+/**
+ * Returns a List object based on the input array.
+ *
+ * @param {Array} array - The input array
+ * @return {List} The List object
+ */
 export function getListObject(array) {
   return List(array);
 }
+/**
+ * Adds an element to the given list.
+ *
+ * @param {Array} list - The list to which the element will be added
+ * @param {any} element - The element to be added to the list
+ * @return {number} The new length of the list after the element is added
+ */
 export function addElementToList(list, element) {
   return list.push(element);
 }
