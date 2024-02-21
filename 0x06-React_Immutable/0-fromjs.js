b494d2a40c80c2391a690a0850dda71c02ba164e
sabrallah91 <sabrallah1991@gmail.com>
2024-02-21 16:07:33 +0100

    Update 0-fromjs.js

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,13 +1,5 @@
 import { fromJS } from 'immutable';
 
-<<<<<<<<<<<<<  ✨ Codeium AI Suggestion  >>>>>>>>>>>>>>
-+/**
-+ * Creates an immutable representation of the input object.
-+ *
-+ * @param {Object} object - The input object to be converted to an immutable representation.
-+ * @return {Immutable} - The immutable representation of the input object.
-+ */
-<<<<<  bot-cab9536f-1ec0-475e-ba96-66bf3a549636  >>>>>
 export default function getImmutableObject(object) {
   return fromJS(object);
 }
