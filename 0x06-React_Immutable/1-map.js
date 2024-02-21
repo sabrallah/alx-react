2e6e9706599a6de7f6a6a0d0d59600561a010ab6
sabrallah91 <sabrallahyoussef@gmail.com>
2024-02-21 15:55:51 +0100

    delate

--- 0x06-React_Immutable/1-map.js
+++
@@ -1,7 +0,0 @@
-import { Map } from 'immutable';
-
-export default function accessImmutableObject(object, array) {
-  const mappedObj = Map(object);
-
-  return mappedObj.getIn(array, undefined);
-}
