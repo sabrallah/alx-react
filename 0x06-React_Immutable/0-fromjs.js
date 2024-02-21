b5f473362082b5a623b0f65f0c23547b87e920b5
sabrallah91 <sabrallah1991@gmail.com>
2024-02-21 16:08:00 +0100

    Update 0-fromjs.js

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,5 +1,11 @@
 import { fromJS } from 'immutable';
 
+/**
+ * Converts a JavaScript object into an Immutable.js object.
+ *
+ * @param {Object} object - the JavaScript object to be converted
+ * @return {Immutable.Map} an Immutable.js object
+ */
 export default function getImmutableObject(object) {
   return fromJS(object);
 }
