501bd41341c159a53c1f25ef15971207ed5f5804
sabrallah youssef <58825588+sabrallah@users.noreply.github.com>
2024-04-08 02:45:01 +0000

    Delete 0x07-react_redux_action_creator_normalizr directory

--- 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/actions/notificationActionTypes.js
+++
@@ -1,9 +0,0 @@
-// Bound the actions
-
-export const MARK_AS_READ = 'MARK_AS_READ';
-export const SET_TYPE_FILTER ='SET_TYPE_FILTER';
-
-export const NotificationTypeFilters = {
-    DEFAULT: 'DEFAULT',
-    URGENT: 'URGENT'
-};
